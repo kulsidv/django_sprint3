dd414052c98cec7d5285d53c8c0d02ab2e93f04d
Kulsitova Daria <dvkulsitova@stud.kpfu.ru>
2025-01-23 17:15:52 +0300

    основное закончила

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -1,6 +1,6 @@
 from django.http import Http404
 from django.shortcuts import render, get_object_or_404
-from datetime import datetime, timezone
+from django.utils import timezone
 
 from .models import Post
 from .models import Category
@@ -8,13 +8,13 @@ from .models import Category
 
 def index(request):
     template = "blog/index.html"
+    now = timezone.now()
     posts = (
         Post.objects.all()
         .filter(
-            pub_date__lte=datetime.now(),
+            pub_date__lte=now,
             is_published=True,
-            category__is_published=True
-        )
+            category__is_published=True)
         .order_by("-pub_date")[0:5]
     )
     content = {"post_list": posts}
@@ -23,28 +23,34 @@ def index(request):
 
 def category_posts(request, category_slug):
     template = "blog/category.html"
+    now = timezone.now()
     category = get_object_or_404(
         Category,
         slug=category_slug,
     )
     if not category.is_published:
         raise Http404(f"Category {category_slug} is invalid.")
-    posts = Post.objects.all().filter(
+    posts = (
+        Post.objects.all()
+        .filter(
+            pub_date__lte=now,
             category=category,
             is_published=True,
-            category__is_published=True,).order_by("-pub_date")
+            category__is_published=True,
+        )
+        .order_by("-pub_date")
+    )
     content = {"category": category, "post_list": posts}
     return render(request, template, content)
 
 
 def post_detail(request, pk):
     template = "blog/detail.html"
+    now = timezone.now()
     post = get_object_or_404(Post, pk=pk)
-    if (
-        post.pub_date > datetime.now()
-        and post.is_published
-        and post.category.is_published
-    ):
+    if (post.pub_date > now 
+        or not post.is_published 
+        or not post.category.is_published):
         raise Http404(f"This post is invalid.")
     content = {"post": post}
     return render(request, template, content)
